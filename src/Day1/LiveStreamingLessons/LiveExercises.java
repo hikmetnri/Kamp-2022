e263dc9055277916379560c8f784e3fac590ab34
hikmet Nuri melemen <hikmet.nuri.33@gmail.com>
2022-09-16 15:50:37 +0300

    Dosyalar ve klasörler oluşturuldu, Canlı yayındaki exersizyapıldı

---
+++ src/Day1/LiveStreamingLessons/LiveExercises.java
@@ -0,0 +1,30 @@
+package Day1.LiveStreamingLessons;
+
+public class LiveExercises {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        String ortaMetin = "İlginizi çekebilir";
+        String altMetin = "Vade Süresi";
+        System.out.println(ortaMetin);
+        int vade = 36;
+        double dolarDun = 18.19;
+        double dolarBugun = 18.19;
+        boolean dolarDustuMu = true;
+        String okYonu = "";
+
+        if (dolarBugun < dolarDun) {
+            okYonu = "down.svg";
+            System.out.println(okYonu);
+        } else if (dolarBugun > dolarDun) {
+            okYonu = "Up.svg";
+            System.out.println(okYonu);
+        } else {
+            okYonu = "equal.svg";
+            System.out.println(okYonu);
+        }
+        String [] krediler= {"Hızlı kredi","Maaşinı halkbaktan","Mutlu Kredi"};
+        for (int i = 0; i < krediler.length ; i++) {
+            System.out.println(krediler[i]);
+        }
+    }
+}
